5cd03851b657ea31d0ff1e4b1e97f786f6edee70
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-08 01:57:49 -0800

    Written my first Ccode

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+vi 5-printf.c
+#include <stdio.h>
+
+/**
+ * *main - Entry point
+ * *Return: Always 0 (Success)
+ * */
+
+int main(void)
+{
+	printf("\'Programming is like building a multilingual puzzle");
+			return (0);
+}
