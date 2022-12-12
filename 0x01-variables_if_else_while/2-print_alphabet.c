21f47388eeca76f3982f7e94bd075dfe7cdbbf41
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-12 05:36:29 -0800

    Letter display in lowercase & uppercase.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Displays lowercase alphabet in a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	
+	char letter;
+	for (letter = "a"; letter <= "z"; letter++)
+	{
+		putchar(letter)
+	}
+	putchar("\n");
+	return (0);
+}
