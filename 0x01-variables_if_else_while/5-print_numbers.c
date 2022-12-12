b92eeb4d1747f21fc49a3ca3a4aa6ef8bd314ff5
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-12 06:15:51 -0800

    Single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Displays single digit with base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	print('\n');
+	return (0);
+}
