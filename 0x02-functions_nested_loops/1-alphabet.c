1ce6d0f1e7ba40f2b40d2baa13392e77891bab26
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-13 11:13:40 -0800

    Printing an alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabets - prints all the letters of the alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	{
+		_putchar(character);
+	}
+	_putchar('\n');
+}
+
+
+
+
