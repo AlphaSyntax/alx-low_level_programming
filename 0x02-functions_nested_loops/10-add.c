e8c3772aedb8af1ee7b86d05c6797d7b7a82c958
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-14 09:18:05 -0800

    Sum of two integer numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integer numbers and outputs the sum.
+ * @num1: operand to be added together with num2.
+ * @num2: operand to be added together with num1.
+ * Return: total of num1 and num2.
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
