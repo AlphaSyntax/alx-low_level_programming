e5c067bf471c92f55aae8fd181a35d5701c01b95
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-16 09:05:31 -0800

    2 integer operands multiplication product

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - multiplies two integer numbers.
  * @num1: first integer number.
  * @num2: second integer number.
- * Return: returns back num1 and num2 sum.
+ * Return: returns back num1 and num2 product.
  */
 
 int mul(int num1, int num2)
