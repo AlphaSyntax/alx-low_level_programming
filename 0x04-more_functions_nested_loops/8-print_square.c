b6cadd7c3fbf0585a7dff2e2fb4f0948f640be23
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-16 10:46:50 -0800

    Output square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - outputs a square.
+ * @area: size of tthe square.
+ *
+ * Return: no value is returned.
+ */
+
+void print_square(int area)
+{
+	int len1, len2;
+
+	if (area > 0)
+	{
+		for (len1 = 0; len2 < area; len1 = len1 + 1)
+		{
+			for (len2 = 0; len2 < (area - 1); len2 = len2 + 1)
+			{
+				putchar('#');
+			}
+			putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n')
+	}
+}
