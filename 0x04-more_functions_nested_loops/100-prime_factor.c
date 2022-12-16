3c2bac22fbc7d71c0f9664c8f9ee0950bf15cc48
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-16 14:44:07 -0800

    Biggest prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * *main - outputs biggest prime factor of a number.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int counter
+	long num = 612852475143;
+
+	while ((counter = counter +1) < (num / 2))
+	{
+		if (0 == (num % counter))
+		{
+			num = num / 2;
+			continue;
+		}
+
+		for (counter = 3; counter < (num / 2); (counter = counter +2))
+		{
+			if (0 == (num % counter))
+			{
+				num = num / counter;
+			}
+		}
+	}
+	printf("%ld\n", num);
+	return (0);
+}
