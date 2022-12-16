822d3272bfa003c0c60dd5fb3ae0382c0eec96bb
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-16 11:06:32 -0800

    Output square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,7 +14,7 @@ void print_square(int area)
 
 	if (area > 0)
 	{
-		for (len1 = 0; (len2 < area); (len1 = len1 + 1))
+		for (len1 = 0; (len1 < area); (len1 = len1 + 1))
 		{
 			for (len2 = 0; (len2 < (area - 1)); (len2 = len2 + 1))
 			{
