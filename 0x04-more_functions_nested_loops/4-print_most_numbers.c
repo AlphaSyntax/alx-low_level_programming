9ae4973e6dd301c220b799244bae7665de0f4d3a
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-16 09:22:31 -0800

    Whole numbers excpt 4 and 2

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - outputs whole numbers up to 9,
+ * excluding 2 and 4.
+ *
+ * Return: nothing is returned.
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num = num + 1)
+	{
+		if ((num == 52) || (n == 50))
+		{
+			continue;
+																	}
+		putchar(num);
+	}
+	putchar(10);
+}
