e557a9f1b7277bbcc070695a1e0983f0cb457c71
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-16 09:51:42 -0800

    Straight line pattern output

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - outputs a line.
+ * @counter: helps keep track of line increase.
+ * Return: no value is returned.
+ */
+
+void print_line(int counter)
+{
+	while ((counter = counter - 1)  > 0)
+	{
+		putchar("_");
+	}
+	putchar("\n");
+}
