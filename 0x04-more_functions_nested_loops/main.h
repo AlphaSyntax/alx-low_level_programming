732127763110b33c3720ebab115e5506619e93b9
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-16 14:23:02 -0800

    Outputs triangle on the terminal

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,7 +12,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void print_square(int size);
 int _putchar(char);
-void print_square(int size);
+void print_triangle(int size);
 int _isupper(int);
 void more_numbers(void);
 int mul(int, int);
