19f2b0069ac4827d0b3dc2d9d88df9340484c949
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-20 11:53:09 -0800

    Resetting a value address

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function accepts pointer to an int as member.
+ * and assigns a value of 98.
+ * @num: input argument
+ * Return: num
+ */
+
+void reset_to_98(int *num)
+{
+	*num = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+void print_array(int *a, int n);
+void rev_string(char *s);
+int _putchar(char c);
+void print_rev(char *s);
+void reset_to_98(int *num);
+void puts_half(char *str);
+void swap_int(int *a, int *b);
+void puts2(char *str);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void _puts(char *str);
+
+#endif /* MAIN_H */
