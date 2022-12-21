eeaa4b5822a90992d097dcc50125e078d6580dc1
“AlphaSyntax” <mamabolobashimane@gmail.com>
2022-12-21 07:18:46 -0800

    An array with number of items

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - display number of items in an array.
+ * @items: number of items in an array.
+ * @list: name of the array.
+ * Return: list with items.
+ *
+ */
+
+void print_array(int *list, int items)
+{
+	int index;
+
+	for (index = 0; index < (items - 1); index++)
+	{
+		printf("%d, ", list[index]);
+	}
+
+	if ((items - 1) == index)
+	{
+		printf("%d", list[items - 1]);
+	}
+	printf('\n');
+}
