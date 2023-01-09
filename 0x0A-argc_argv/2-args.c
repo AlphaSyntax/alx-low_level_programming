518b0d81c3010537a5d1290e07ed71ba01e759f3
“AlphaSyntax” <mamabolobashimane@gmail.com>
2023-01-09 12:05:17 -0800

    Outputs all arguments it recieves

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  main - outputs all arguments passed into the program.
+ *  @argc: number of arguments.
+ *  @argv: array of arguments.
+ *  Return: Always 0 (Success).
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
+	return (0);
+}
