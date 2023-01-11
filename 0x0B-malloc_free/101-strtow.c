963dc81c903eb87143ed54e96bf97ea15775dc20
“AlphaSyntax” <mamabolobashimane@gmail.com>
2023-01-11 07:34:25 -0800

    Splitting strings into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * count_word - counter the words in a string.
+ * @s: string to evaluate.
+ * Return: number of words.
+ */
+
+int count_word(char *s)
+{
+	int extract, coun, word;
+
+	extract, word = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		if (s[count] == ' ')
+			extract = 0;
+		else if (extract == 0)
+			extract = 1;
+			word = word + 1;
+	}
+	return (word);
+}
+
+/**
+ * **strtow - splits a string into words.
+ * @str: string to split
+ * Return: pointer to an array of strings (Success).
+ *  or NULL (Error).
+ */
+
+char **strtow(char *str)
+{
+	char **matrix, *temp;
+
+	int i, begin, last, strings, j = 0, length = 0, count = 0;
+
+	while (*(str + length))
+		length = length + 1;
+	strings = count_word(str);
+	if (strings == 0)
+		return (NULL);
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i <= length; i++)
+	{
+		if (str[i] == '' || str[i] == '\0')
+			if (count)
+				last = i;
+				temp = (char *) malloc(sizeof(char) * (count + 1));
+				if (temp == NULL)
+					return (NULL);
+				while (begin < last)
+				{
+					*temp++ = str[begin++];
+				}
+				*temp = '\0';
+				matrix[j] = temp - count;
+				j = j + 1;
+				count = 0;
+		else if (count++ == 0)
+			begin = i;
+	}
+	matrix[j] = NULL;
+	return (matrix);
+}
